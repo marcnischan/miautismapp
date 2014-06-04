c9c6a077ed2b7f93091103eaa165a897da38f326
Marc Nischan <marcnischan@gmail.com>
2014-06-03 20:34:45 -0400

    changed the locations service to get the reviews page working again.

--- app/js/controllers/ReviewCtrl.js
+++ app/js/controllers/ReviewCtrl.js
@@ -1,10 +1,12 @@
 App.controller('ReviewCtrl', ['$scope', '$stateParams', 'locations', function($scope, $stateParams, locationService) {
 
+
     if ($stateParams.locationId) {
-        locationService.getLocation($stateParams, $stateParams.locationId, function(review, location) {
-            $scope.location = location;
-            $scope.business = business;
-            console.log('i hope this says ' + location.id);
+        locationService.getLocation($stateParams.locationId, function(location) {
+            $scope.business = location.business;
+            $scope.reviewer = location.reviewer;
+
         });
     }
 }]);
+
