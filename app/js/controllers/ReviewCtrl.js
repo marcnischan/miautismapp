c65b8efbfc8fb738c95f749543db3884ae359d37
Dan Trenz <dtrenz@gmail.com>
2014-06-11 22:59:24 -0400

    Review Screen: Business Map Location Map on review screen is centered on, and has a marker for the current business, if the business has a lat/lng coordinates.

--- app/js/controllers/ReviewCtrl.js
+++ app/js/controllers/ReviewCtrl.js
@@ -1,9 +1,21 @@
 App.controller('ReviewCtrl', ['$scope', '$stateParams', 'locations', function($scope, $stateParams, locationService) {
     if ($stateParams.locationId) {
+        $scope.map = {
+            center: {
+                latitude: 0,
+                longitude: 0
+            }
+        };
+
         locationService.getLocation($stateParams.locationId, function(location) {
-            $scope.business = location.business;
+            var business = location.business;
+
+            $scope.business = business;
             $scope.reviewer = location.reviewer;
 
+            if (business.coords) {
+                $scope.map.center = business.coords;
+            }
         });
     }
 }]);
